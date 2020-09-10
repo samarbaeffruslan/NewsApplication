2a10d69b7cf1b821f639e00d37240a64c25cfc87
ruslan <Samarbaev36@gmail.com>
2020-09-11 01:17:27 +0300

    search news

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "ba31fe3632784b6ba78ea818e866b1b8"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
